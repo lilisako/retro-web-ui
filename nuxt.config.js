11c7e7299dd2f68c4eb932aabfa6d27b789c9eac
risako <lilisako062397@gmail.com>
2022-04-02 22:38:50 +0900

    feat: add seo

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,8 +8,14 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: 'Welcome to Web Design History! Explore the websites from the various range of eras! ' },
+      { hid: 'keywords', name: 'keywords', content: 'web, design, history, aesthetic, 90s, 00s' },
+      { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Web Design History' },
+      { hid: 'og:title', property: 'og:title', content: 'Web Design History' },
+      { hid: 'og:description', property: 'og:description', content: 'Welcome to Web Design History! Explore the websites from the various range of eras!' },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
+      { hid: 'og:image', property: 'og:image', content: 'https://user-images.githubusercontent.com/33516104/161385849-4925fda7-c72d-4eeb-9f3d-c8c71b8e94c4.png' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
