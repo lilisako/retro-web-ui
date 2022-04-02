93f96cf4da90bbd0aed7e9e0a64ecf810781f1f2
risako <lilisako062397@gmail.com>
2022-04-02 21:13:11 +0900

    feat: add components

---
+++ components/facebook-2005.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="flex justify-center my-2">
+    <div>
+      <img src="~/assets/facebook-2005/pageheaderbg.gif">
+      <div class="flex">
+        <div class="w-[132px]">
+          <p class="text-sm text-[#919191]">
+            Email:
+          </p>
+          <input type="text" size="14" class="border border-[#D8DFEA]">
+          <p class="text-sm text-[#919191]">
+            Password:
+          </p>
+          <input type="text" size="14" class="border border-[#D8DFEA]">
+          <div class="flex justify-end my-1">
+            <button class="px-1 mx-1 text-xs text-white bg-[#3B5998]">
+              Login
+            </button>
+            <button class="px-1 mx-1 text-xs text-white bg-[#3B5998]">
+              Register
+            </button>
+          </div>
+        </div>
+        <div class="w-[628px]">
+          <div class="border border-[#6D84B5]">
+            <div class="px-3 bg-[#6D84B5]">
+              <p class="font-bold text-white">
+                Welcome to the Facebook!
+              </p>
+            </div>
+            <div class="p-4 w-full leading-loose">
+              <p class="py-1 text-xs font-bold">
+                The Facebook is an online directory that connects people through social networks at schools.
+              </p>
+              <p class="py-1 text-xs">
+                Now there are two Facebooks: one for people in college and one for people in high school.
+              </p>
+              <p class="py-1 text-xs">
+                The site is open to a lot of schools, but not everywhere yet. We're working on it.
+              </p>
+              <p class="py-1 text-xs">
+                You can use Facebook to:
+              </p>
+              <ul class="px-3 list-disc">
+                <li class="text-xs">
+                  Look up people at your school.
+                </li>
+                <li class="text-xs">
+                  See how people know each other.
+                </li>
+                <li class="text-xs">
+                  Find people in your classes and groups.
+                </li>
+              </ul>
+              <div class="flex justify-center my-1">
+                <button class="px-1 mx-1 text-xs text-white bg-[#3B5998]">
+                  Login
+                </button>
+                <button class="px-1 mx-1 text-xs text-white bg-[#3B5998]">
+                  Register
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="w-full text-center">
+            <div class="flex justify-center items-center">
+              <p class="mx-2 text-xs text-[#6D84B5]">
+                about
+              </p>
+              <p class="mx-2 text-xs text-[#6D84B5]">
+                jobs
+              </p>
+              <p class="mx-2 text-xs text-[#6D84B5]">
+                announce
+              </p>
+              <p class="mx-2 text-xs text-[#6D84B5]">
+                advertise
+              </p>
+              <p class="mx-2 text-xs text-[#6D84B5]">
+                terms
+              </p>
+              <p class="mx-2 text-xs text-[#6D84B5]">
+                privacy
+              </p>
+            </div>
+            <p class="text-xs">
+              a Mark Zuckerberg production
+            </p>
+            <p class="text-xs">
+              Facebook © 2005
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
